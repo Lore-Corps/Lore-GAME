f8bcf42c41c627a4ae7f660d2a5faf2881725d8c
oxokaneoxo <kane.miller@hotmail.com>
2022-07-08 14:11:33 -0700

    Update EnemyEntity.gd
    Stats are now randomized when enemy is created. Before it was just on creation

--- Scripts/EnemyEntity.gd
+++ Scripts/EnemyEntity.gd
@@ -4,11 +4,7 @@ var rng = RandomNumberGenerator.new()
 
 func _ready():
 	rng.randomize()
-	agility = rng.randi_range(5, 20)
-	intelligence = rng.randi_range(5, 20)
-	strength = rng.randi_range(5, 20)
-	max_health = rng.randi_range(50, 200)
-	curr_health = max_health
+	randomize_stats()
 	char_name = "Brad the Madlad"
 	print("Here I am ", char_name, "!")
 	update_entity_label()
@@ -16,5 +12,12 @@ func _ready():
 
 func reset():
 	.reset()
+	randomize_stats()
+
+func randomize_stats():
 	rng.randomize()
-	agility = rng.randi_range(5, 15)
+	agility = rng.randi_range(5, 20)
+	intelligence = rng.randi_range(5, 20)
+	strength = rng.randi_range(5, 20)
+	max_health = rng.randi_range(50, 200)
+	curr_health = max_health
