d264a178b1195c8aad72f5d7b69c2f0944d37de4
oxokaneoxo <kane.miller@hotmail.com>
2022-07-13 19:10:52 -0700

    Cleaned up the TargetSpawningMinigame prefab,
    Also added some TODOs and comments

--- Scripts/PlayerTarget.gd
+++ Scripts/PlayerTarget.gd
@@ -3,10 +3,10 @@ extends Node2D
 
 signal target_destroyed
 
-
+# TODO maybe add a velocity here to make them move in a random dirrection
+# or have them have custom sizes
 func _ready() -> void:
 	pass
-
 
 
 func _on_TargetButton_pressed() -> void:
--- Scripts/TargetSpawningController.gd
+++ Scripts/TargetSpawningController.gd
@@ -1,5 +1,6 @@
 extends Node
 
+
 export(PackedScene) var player_target_node
 
 var target: Node
@@ -21,16 +22,20 @@ func _ready() -> void:
 func _process(delta) -> void:
 	timer -= delta
 	if game_running:
-		
+		# Spawns a target at random intervals
 		if timer < 0:
 			create_target()
 			timer = rng.randf_range(0.5, 0.7)
-			
+			# easy way to keep track of how maybe targets are active. 
+			# Scene currently has a button to start the game, so we -1
 		number_of_active_targets = get_child_count() -1
 		if number_of_active_targets > 10:
 			end_target_minigame()
 			
 
+# Does what it says.
+# TODO add a negitive if the player fails. The idea we are thinking about is using
+# this as a status effect and having the player take big damage on failure.
 func end_target_minigame() -> void:
 	print("You suck")
 	game_running = false
@@ -40,7 +45,9 @@ func end_target_minigame() -> void:
 			child.queue_free()
 	
 
-
+# Spawns a target at a random location. 
+# TODO maybe make them move at random aswell and Make it not spawn on top of the
+# attack and delayed attack button. 
 func create_target() -> void:
 	target = player_target_node.instance()
 	add_child(target)
@@ -48,7 +55,8 @@ func create_target() -> void:
 	target.connect("target_destroyed", self, "_on_PlayerTarget_target_destroyed")
 	print("We created a Target")
 
-
+# The child button starts the minigame right now, this will be removed in the future
+# when we make this start via the enemyEnitity
 func _on_StartTargetSpawningButton_pressed() -> void:
 	if number_of_targets_destroyed != 0:
 		reset_target_minigame()
