72b06baf59e3f126db1827ae537fce9846e0a0c4
oxokaneoxo <kane.miller@hotmail.com>
2022-08-16 14:05:16 -0700

    PlayerTargets have been updated
    They now use less resources. POGGERS

--- Scripts/PlayerTarget.gd
+++ Scripts/PlayerTarget.gd
@@ -24,7 +24,7 @@ var random_scale: float
 # or have them have custom sizes
 func _ready() -> void:
 	rng.randomize()
-	random_speed = rng.randf_range(-4, 4)
+	random_speed = rng.randf_range(-400, 400)
 	random_scale = rng.randf_range(1, 2)
 	self.scale.x = random_scale 
 	self.scale.y = random_scale 
@@ -33,17 +33,15 @@ func _ready() -> void:
 	self.modulate = Color(rng.randf_range(0, 1),rng.randf_range(0, 1),rng.randf_range(0, 1))
 
 
-func _process(_delta) -> void:
+func _process(delta) -> void:
 	
-	position += Vector2(move_speed_x, move_speed_y)
-
+	position += Vector2(move_speed_x, move_speed_y) * delta
+	
 	position.x = wrapf(position.x, screen_min_x, screen_max_x)
 	position.y = wrapf(position.y, screen_min_y, screen_max_y)
 
 
-
-
-func _on_KinematicBody2D_input_event(_viewport, event, _shape_idx):
+func _on_PlayerTarget_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		emit_signal("target_destroyed")
 		self.queue_free()
